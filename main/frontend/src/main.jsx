e522333c44d22f2d398808bc64f7f0431f480681
ZXCDani2502 <ZXCDani2502@users.noreply.github.com>
2024-12-18 22:34:17 +0200

    settings button and links

--- main/frontend/src/main.jsx
+++ main/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route, Routes} from 'react-router'
+import { BrowserRouter as Router, Route, Routes} from 'react-router'
 import HomePage from './pages/HomePage'
 import axios from 'axios'
 
@@ -9,10 +9,17 @@ axios.defaults.baseURL = 'http://localhost:8080/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/Login' element={<HomePage/>}/>
+        {/* <Route path='/Logout' element={}/>
+        <Route path='/Signup' element={}/>
+        <Route path='/Account' element={}/>
+        <Route path='/Games' element={}/>
+        <Route path='/Games/Chess' element={}/>
+        <Route path='/Games/Backgammon' element={}/> */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 )
