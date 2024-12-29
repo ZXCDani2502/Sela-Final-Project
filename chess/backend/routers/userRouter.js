2bd3064061c36a462e069c40acedf4358b60858b
ZXCDani2502 <danielg2502@gmail.com>
2024-12-29 12:32:40 +0200

    converted to typescript and removed unrelated folders

--- chess/backend/routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getAll, addUser } from "../controllers/userController.js"
-const router = express.Router()
-
-router.route("/get").get(getAll)
-router.route("/add").post(addUser)
-
-export default router
