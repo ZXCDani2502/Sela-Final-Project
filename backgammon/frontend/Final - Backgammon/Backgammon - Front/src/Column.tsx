1f5741add680e3df44c8997328695ea1ef5d0252
ZXCDani2502 <ZXCDani2502@users.noreply.github.com>
2025-01-14 02:01:42 +0200

    Board 100% working!

--- backgammon/frontend/Final - Backgammon/Backgammon - Front/src/Column.tsx
+++ backgammon/frontend/Final - Backgammon/Backgammon - Front/src/Column.tsx
@@ -13,23 +13,35 @@ export function Column({ column, pieces, quadrant }: ColumnProps) {
         id: column.id,
     })
     return (
-        <div key={column.id} className='w-[64px] h-full relative'>
+        <div ref={setNodeRef} key={column.id} className={`relative w-[64px] h-full flex items-center ${quadrant[0] === 't' ? 'flex-col' : 'flex-col-reverse'}`}>
             <div
-                ref={setNodeRef}
-                className={`absolute flex w-0 h-0 border-l-[32px] border-l-transparent border-r-[32px] border-r-transparent 
-                    ${
-                        quadrant[0] === 't'
-                            ? `top-0 border-t-[250px] flex flex-col justify-end
-                                ${column.id % 2 === 0 ? 'border-t-amber-700' : 'border-t-amber-300'}`
-                            : `bottom-0 border-b-[250px] flex flex-col justify-start 
-                                ${column.id % 2 === 0 ? 'border-b-amber-700' : 'border-b-amber-300'}`
-                    } `}>
-                <div className={`flex ${quadrant[0] === 't' ? 'flex-col-reverse' : 'flex-col'} items-center z-10`}>
-                    {pieces.map((piece) => (
-                        <Piece key={piece.id} piece={piece} />
-                    ))}
-                </div>
-            </div>
+                className={`absolute border-l-[32px] border-l-transparent border-r-[32px] border-r-transparent opacity-35 ${
+                    quadrant[0] === 't'
+                        ? `top-0 border-t-[250px] flex flex-col justify-end
+                        ${column.id % 2 === 0 ? 'border-t-amber-700' : 'border-t-amber-300'}`
+                        : `bottom-0 border-b-[250px] flex flex-col justify-start
+                        ${column.id % 2 === 0 ? 'border-b-amber-700' : 'border-b-amber-300'}`
+                } `}></div>
+            {pieces.map((piece) => (
+                <Piece key={piece.id} piece={piece} />
+            ))}
         </div>
+
+        // <div
+        //     ref={setNodeRef}
+        //     className={`absolute flex w-0 h-0 border-l-[32px] border-l-transparent border-r-[32px] border-r-transparent
+        //         ${
+        //             quadrant[0] === 't'
+        //                 ? `top-0 border-t-[250px] flex flex-col justify-end
+        //                     ${column.id % 2 === 0 ? 'border-t-amber-700' : 'border-t-amber-300'}`
+        //                 : `bottom-0 border-b-[250px] flex flex-col justify-start
+        //                     ${column.id % 2 === 0 ? 'border-b-amber-700' : 'border-b-amber-300'}`
+        //         } `}>
+        //     <div className={`flex ${quadrant[0] === 't' ? 'flex-col-reverse' : 'flex-col'} items-center z-10`}>
+        //         {pieces.map((piece) => (
+        //             <Piece key={piece.id} piece={piece} />
+        //         ))}
+        //     </div>
+        // </div>
     )
 }
