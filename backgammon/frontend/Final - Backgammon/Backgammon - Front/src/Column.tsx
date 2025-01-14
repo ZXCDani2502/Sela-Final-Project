fcdaa1b4f298abd4976ee939582d2ad6d42f5700
ZXCDani2502 <danielg2502@gmail.com>
2025-01-14 10:07:37 +0200

    removed commented code

--- backgammon/frontend/Final - Backgammon/Backgammon - Front/src/Column.tsx
+++ backgammon/frontend/Final - Backgammon/Backgammon - Front/src/Column.tsx
@@ -26,22 +26,5 @@ export function Column({ column, pieces, quadrant }: ColumnProps) {
                 <Piece key={piece.id} piece={piece} />
             ))}
         </div>
-
-        // <div
-        //     ref={setNodeRef}
-        //     className={`absolute flex w-0 h-0 border-l-[32px] border-l-transparent border-r-[32px] border-r-transparent
-        //         ${
-        //             quadrant[0] === 't'
-        //                 ? `top-0 border-t-[250px] flex flex-col justify-end
-        //                     ${column.id % 2 === 0 ? 'border-t-amber-700' : 'border-t-amber-300'}`
-        //                 : `bottom-0 border-b-[250px] flex flex-col justify-start
-        //                     ${column.id % 2 === 0 ? 'border-b-amber-700' : 'border-b-amber-300'}`
-        //         } `}>
-        //     <div className={`flex ${quadrant[0] === 't' ? 'flex-col-reverse' : 'flex-col'} items-center z-10`}>
-        //         {pieces.map((piece) => (
-        //             <Piece key={piece.id} piece={piece} />
-        //         ))}
-        //     </div>
-        // </div>
     )
 }
