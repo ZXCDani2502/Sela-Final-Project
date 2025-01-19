f25348c40b65fb5d15846fab061e9e57fa5a1319
ZXCDani2502 <ZXCDani2502@users.noreply.github.com>
2025-01-19 13:51:43 +0200

    final commit

--- frontend/src/hooks/useLookingForMatch.ts
+++ frontend/src/hooks/useLookingForMatch.ts
@@ -14,28 +14,6 @@ const useLookingForMatch = () => {
             setLoading(false)
         }
     }
-
-    // const sendMove = async (move: Move) => {
-    //     setLoading(true)
-    //     try {
-    //         const res = await fetch(`http://localhost:6600/api/chess/send/`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({ move }),
-    //         })
-    //         const data = await res.json()
-    //         if (data.error) throw new Error(data.error)
-
-    //         setMove(data.move)
-    //     } catch (error: any) {
-    //         toast.error(error.message)
-    //     } finally {
-    //         setLoading(false)
-    //     }
-    // }
-
     return { loading }
 }
 
